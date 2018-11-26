ea22c084dba832d9bd68dabe3d65e3b6ba00677a
kanontang <kanontang@gmail.com>
2018-11-26 10:55:28 +0800

    增加上下动画

--- src/motion/Slide.ts
+++ src/motion/Slide.ts
@@ -6,7 +6,7 @@ module motion
 export class Slide
 {
 	/**
-	 * 创建左侧飞入过渡
+	 * 创建飞入左侧过渡
 	 * @param duration 过渡间隔
 	 * @param ease 过渡缓动效果
 	 */
@@ -33,7 +33,7 @@ export class Slide
 	}
 
 	/**
-	 * 创建右侧飞入过渡
+	 * 创建飞入右侧过渡
 	 * @param duration 过渡间隔
 	 * @param ease 过渡缓动效果
 	 */
@@ -55,6 +55,60 @@ export class Slide
 				oldScreen.y = 0;
 				Laya.Tween.clearAll(oldScreen);
 				Laya.Tween.to(oldScreen, {x:oldScreen.width}, duration, ease);
+			}
+		};
+	}
+
+	/**
+	 * 创建飞入顶部过渡
+	 * @param duration 过渡间隔
+	 * @param ease 过渡缓动效果
+	 */
+	public static createSlideTopTransition(duration:number = 400, ease:Function=Laya.Ease.sineOut):Function
+	{
+		return function(oldScreen:Laya.Sprite, newScreen:Laya.Sprite, onComplete:Function)
+		{
+			if(!oldScreen && !newScreen) return;
+			if(newScreen)
+			{
+				newScreen.x = 0;
+				newScreen.y = newScreen.height;
+				Laya.Tween.clearAll(newScreen);
+				Laya.Tween.to(newScreen, {y:0}, duration, ease, Handler.create(this, onComplete));
+			}
+			if(oldScreen)
+			{
+				oldScreen.x = 0;
+				oldScreen.y = 0;
+				Laya.Tween.clearAll(oldScreen);
+				Laya.Tween.to(oldScreen, {y:-newScreen.height}, duration, ease);
+			}
+		};
+	}
+
+	/**
+	 * 创建飞入底部过渡
+	 * @param duration 过渡间隔
+	 * @param ease 过渡缓动效果
+	 */
+	public static createSlideBottomTransition(duration:number = 400, ease:Function=Laya.Ease.sineOut):Function
+	{
+		return function(oldScreen:Laya.Sprite, newScreen:Laya.Sprite, onComplete:Function)
+		{
+			if(!oldScreen && !newScreen) return;
+			if(newScreen)
+			{
+				newScreen.x = 0;
+				newScreen.y = -newScreen.height;
+				Laya.Tween.clearAll(newScreen);
+				Laya.Tween.to(newScreen, {y:0}, duration, ease, Handler.create(this, onComplete));
+			}
+			if(oldScreen)
+			{
+				oldScreen.x = 0;
+				oldScreen.y = 0;
+				Laya.Tween.clearAll(oldScreen);
+				Laya.Tween.to(oldScreen, {y:newScreen.height}, duration, ease);
 			}
 		};
 	}
